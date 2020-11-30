2402805cf767a618246f099b2dc91366d9a96f1f
ElMitch <53499368+ElMitch@users.noreply.github.com>
2020-11-30 14:36:22 -0600

    Initial Commit

---
+++ Prueba Nextia MitchTests/Prueba_Nextia_MitchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Prueba_Nextia_MitchTests.swift
+//  Prueba Nextia MitchTests
+//
+//  Created by Mitch Samaniego on 30/11/20.
+//
+
+import XCTest
+@testable import Prueba_Nextia_Mitch
+
+class Prueba_Nextia_MitchTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
